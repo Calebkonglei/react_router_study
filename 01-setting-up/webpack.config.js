eedc81bd172d79b8fc1ac10476b1037c10fd5c7e
dannisi <18296155494@163.com>
2016-06-02 16:51:41 +0800

    Add files via upload

---
+++ 01-setting-up/webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: './index.js',
+
+  output: {
+    path:__dirname,
+    filename:'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      { 
+        test: /\.jsx?$/, 
+        exclude: /node_modules/, 
+        loader: 'babel',
+        query:{
+          presets:['es2015','react']
+        }
+      }
+    ]
+  }
+}
