c5ec513c1e02ba1ae703c3b74cf66ef0ebebdebf
dannisi <18296155494@163.com>
2016-06-02 17:24:18 +0800

    Add files via upload

--- 01-setting-up/modules/App.js
+++ 01-setting-up/modules/App.js
@@ -1,7 +1,16 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import { Link } from 'react-router';
 
 export default class App extends Component{
   render() {
-    return <div>Hello, React Router!</div>
+    return (
+    	<div>
+    	   <h1>Hello, React Router!</h1>
+    	   <ul role="nav">
+    	   	  <li><Link to="/about">About</Link></li>
+    	   	  <li><Link to="/repos">Repos</Link></li>
+    	   </ul>  
+    	</div>
+    )  
   }
 }
