97b0706556e1329898aad8ff37211eb252b21a9c
dannisi <18296155494@163.com>
2016-06-02 21:41:59 +0800

    Add files via upload

--- 01-setting-up/modules/App.js
+++ 01-setting-up/modules/App.js
@@ -1,14 +1,13 @@
 import React, {Component} from 'react';
-import { Link } from 'react-router';
-
+import NavLink from './NavLink';
 export default class App extends Component{
   render() {
     return (
     	<div>
     	   <h1>Hello, React Router!</h1>
     	   <ul role="nav">
-    	   	  <li><Link to="/about">About</Link></li>
-    	   	  <li><Link to="/repos">Repos</Link></li>
+    	   	  <li><NavLink to="/about">About</NavLink></li>
+    	   	  <li><NavLink to="/repos">Repos</NavLink></li>
     	   </ul>  
     	</div>
     )  
